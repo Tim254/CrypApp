a9eae9ba37b8e8ff6b5f0437cf8c20efa1969d81
Tim254 <tim.mailu@gmail.com>
2022-06-24 17:49:08 +0300

    loop over the currency elements to display on dropdown

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,18 @@ const API_URL = "https://api.exchangerate-api.com/v4/latest/USD"
 let html = '';
 
 async function currency() {
-    const res = await fetch (API_URL)
-    const data = await res.json();
+    const response = await fetch (API_URL)
+    const data = await response.json();
     console.log(data)
     const arrKeys = Object.keys(data.rates)
-    console.log(arrKeys)
+    const rates = data.rates;
+    console.log(rates)
+    arrKeys.map(item => {
+        return html += `<option value=${item}>${item}</option>`;
+    })
+    // console.log(html);
+    for(let i=0; i<select.length; i++){
+        select[i].innerHTML = html;
+    }
 }
 currency();
